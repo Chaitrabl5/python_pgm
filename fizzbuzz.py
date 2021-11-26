ce89b5aaa20e96396a53dbef59b7a9ddb3dbaff7
Chaitra <blchaitra58@gmail.com>
2021-11-26 21:21:44 +0530

    commit

---
+++ fizzbuzz.py
@@ -0,0 +1,26 @@
+#Write a short program that prints each number from 1 to 100 on a new line. 
+
+#For each multiple of 3, print "Fizz" instead of the number. 
+
+#For each multiple of 5, print "Buzz" instead of the number. 
+
+#For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+def fizzbuzz(n):
+   i=1
+   while i <= n:
+        if i%3==0:
+          print("Fizz", end="")
+          if i%5==0:
+                print("Buzz", end="")
+        elif i%5==0:
+             print("Buzz", end="")
+        else:
+            print(i, end="")
+        print()
+        i+=1
+
+inputnum=int(input("enter the number"))
+fizzbuzz(inputnum)
+
+
+
